d43fe6a01e2df6dcd790355eb09233c9a6a7eab7
suhara beevi <sairasuhara12345@gmail.com>
2023-07-06 23:09:24 +0530

    first commit

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>
+     <h1 className='bg-info text-white display'>Products</h1>
+    </div>
+  )
+}
+
+export default Products
